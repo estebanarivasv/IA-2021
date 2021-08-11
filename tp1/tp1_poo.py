16d68ec8120bc8ec0d9e53c7c1e0acf99f5a1e04
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2021-08-11 20:35:16 -0300

    primer aspiradora, código comentado

--- tp1/tp1_poo.py
+++ tp1/tp1_poo.py
@@ -1,5 +1,6 @@
 
 import random
+import time
 
 choices = [" ", "+", "*", "#"]
 
@@ -30,27 +31,43 @@ class Vacuum:
 
 	def run(self):
 		while True:
-			while self.position >= 0 and self.position <= self.floor.size:
-				print(self.position)
+			# Esta condición se cumple mientras la aspiradora NO esté en los extremos.
+			# Cuando llega a un extremo, sale del while y comprueba el cambio de dirección
+			while self.position > 0 and self.position < self.floor.size -1:
+				print('		\033[92m Tile Nº:' + str(self.position))
 				tile = self.floor.tiled[self.position]
+				# Si está limpio no hace nada, solo se mueve
 				if tile.state == ' ' :
+					print('\033[92m Its clean')
 					self.move()
+				# Si esta sucio llama al método limpiar y se mueve
 				else:
+					print('\033[91m Its dirty')
 					tile.state = self.clean(tile.state)	
 					self.move()
 				print(tile.state)
+				time.sleep(2)
+
 
 			if self.position <= 0:
-				self.position = 0
+				self.position = 1
 				self.changeDirection()
-			elif self.position >= self.floor.size:
-				self.position = self.floor.size
+			elif self.position >= self.floor.size -1:
+				self.position = self.floor.size -2
 				self.changeDirection()
+			time.sleep(3)
+			print(' ')
+			print('\033[1m \033[92m -----------------')
+			print('\033[1m \033[92m Restart')
+			print('\033[1m \033[92m -----------------')
+			print(' ')	
+
+
 
 
 
 	def clean(self, state):
-		print('se limpió')
+		print('Cleaning')
 		if state == '+':
 			return ' '
 		elif state == '*':
@@ -61,28 +78,30 @@ class Vacuum:
 
 
 	def move(self):
-		print('se movió')
 		if self.direction == 'r':
-			self.position = self.position + 1
+			self.position = self.position + 1			
+			print('\033[94m Moves to tile Nº:' + str(self.position))
 		elif self.direction == 'l':
 			self.position = self.position - 1
+			print('\033[94m Moves to tile Nº:' + str(self.position))
 
 	def changeDirection(self):
-		print('cambió de dirección')
+		print('\033[93m _______________')
+		print('\033[93m Wall detected')
+		print('\033[93m _______________')
 		if self.direction == 'l':
 			self.direction = 'r'
 		elif self.direction == 'r':
-			self.direction = 'l'		
-		# if self.position == self.floor.size:
-		# 	self.direction = 'l'
-		# elif self.position == 0:
-		# 	self.direction = 'r'
+			self.direction = 'l'
 
 
 if (__name__ == "__main__"):
 
-	floor = Floor(N=10)
+	size = random.randint(1,100)
+	position = random.randint(0, size-1)
 
-	vacuum = Vacuum(floor=floor, direction='l', position=5)
+	floor = Floor(N=size)
+
+	vacuum = Vacuum(floor=floor, direction='l', position=position)
 	vacuum.run()
 
