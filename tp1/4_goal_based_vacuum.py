095cd1e3424ba7d2a7228efd696ad426ed46a8aa
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2021-08-17 19:33:55 -0300

    ejercicio 4

---
+++ tp1/4_goal_based_vacuum.py
@@ -0,0 +1,207 @@
+import random
+import time
+
+choices = [" ", "+", "*", "#"]
+dirt = ["+", "*", "#"]
+
+
+class Tile:
+
+	def __init__(self,choices):
+		self.state = random.choice(choices)
+		# self.state = state
+		self.clean_happened = 0
+		self.is_stained = False
+
+
+class Floor:
+
+	def __init__(self, N):
+		self.size = N
+		self.tiles = [Tile(choices) for x in range(self.size)]
+		# self.tiles = [
+		# 	Tile('*'),
+		# 	Tile('*'),
+		# 	Tile('*'),
+		# 	Tile('+'),
+		# 	Tile('#'),
+		# 	Tile('+'),
+		# 	Tile('*'),
+		# 	Tile('*'),
+		# 	Tile(' '),
+		# 	Tile('*')
+		# ]
+
+	def print_tiles(self):
+		states = []
+		for x in self.tiles:
+			states.append(x.state)
+		print("\n\033[1m\033[33mActual floor state: ", states)
+		print('\033[92m')
+
+	def ramdon_dirt(self):
+		new_tile_position = random.randint(0, self.size - 1)
+		if self.tiles[new_tile_position].state == '#':
+			pass
+		else:
+			self.tiles[new_tile_position] = Tile(dirt)
+				
+
+
+
+class Vacuum:
+
+	def __init__(self, floor, direction, position):
+		self.floor = floor
+		self.position = position
+		self.direction = self.cleaning_percentage()
+		# print(self.direction)
+		self.num_tiles = floor.size
+		self.movements = 0
+		self.cleaned_tiles = 0
+
+	def run(self):
+		print("Number of tiles: ", self.num_tiles)
+
+		while self.cleaned_tiles < self.num_tiles:
+
+			# Esta condición se cumple mientras la aspiradora NO esté en los extremos.
+			# Cuando llega a un extremo, sale del while y comprueba el cambio de dirección.
+
+			while 0 <= self.position <= self.floor.size - 1:
+				self.floor.print_tiles()
+				self.print_position()
+
+				self.movements += 1
+				print("Movement No: ", self.movements)
+
+				print('\n\033[92mTile Nº:' + str(self.position + 1))
+				tile = self.floor.tiles[self.position]
+				print("Initial tile state: ", tile.state)
+
+				# Si está limpio no hace nada, solo se mueve
+				if tile.state == ' ' or (tile.is_stained is True and tile.clean_happened > 3):
+					self.cleaned_tiles += 1
+					print('Cleaned tiles: ', self.cleaned_tiles, '/', self.num_tiles)
+					print('\033[92mIts clean')
+					if tile.is_stained is True:
+						print("Times that the tile has been cleaned: ", tile.clean_happened)
+
+					self.move()
+				# Si esta sucio llama al método limpiar y se mueve
+				else:
+					print('\033[91mIts dirty')
+					tile = self.clean(tile)
+					self.move()
+				print("New tile state: '", tile.state, "'")
+				time.sleep(1)
+				self.floor.ramdon_dirt()
+
+			if self.position <= -1:
+				# Cuando se salga del extremo izquierdo, que lo vuelva a posicionar en la posicion 0
+				self.position = 0
+				self.change_direction()
+			elif self.position >= self.floor.size:
+				# Cuando se salga del extremo derecho, que lo vuelva a posicionar en la última posicion
+				self.position = self.floor.size - 1
+				self.change_direction()
+			time.sleep(1)
+
+			print(' ')
+			print('\033[1m \033[92m -----------------')
+			print('\033[1m \033[92m Restart')
+			print('\033[1m \033[92m -----------------')
+			print(' ')
+			
+
+		print("Final state: ", self.floor.print_tiles())
+
+	def print_position(self):
+		position = []
+		for x in self.floor.tiles:
+			position.append(x.clean_happened)
+		position[self.position] = "V"
+		print("Vacuum position and times cleaned: ", position)
+
+	def clean(self, tile):
+		print('Cleaning')
+		# El entorno se modifica pero la aspiradora realmente no reconoce los tipos de suciedad
+		if tile.state == '+':
+			tile.state = ' '
+		elif tile.state == '*':
+			tile.state = '+'
+		elif tile.state == '#':
+			tile.state = '#'
+		tile.clean_happened += 1
+
+		# Si la baldosa sigue sucia, y ya fue limpiada, se marca como manchada.
+		if tile.state == '#' and tile.clean_happened < 4:
+			tile.is_stained = True
+
+		return tile
+
+	def move(self):
+		if self.direction == 'r':
+			self.position = self.position + 1
+			print('\033[94mMoves to tile Nº:' + str(self.position + 1))
+		elif self.direction == 'l':
+			self.position = self.position - 1
+			print('\033[94mMoves to tile Nº:' + str(self.position + 1))
+
+	def change_direction(self):
+		print('\033[93m _______________')
+		print('\033[93m Wall detected')
+		print('\033[93m _______________')
+		if self.direction == 'l':
+			self.direction = 'r'
+		elif self.direction == 'r':
+			self.direction = 'l'
+
+	def cleaning_percentage(self):
+		left_array = self.floor.tiles[0:self.position]
+		right_array = self.floor.tiles[self.position:self.floor.size]
+		left_percent = self.count_clean(left_array)
+		rigth_percent = self.count_clean(right_array)
+
+		if left_percent <= rigth_percent:
+			return 'l'
+		else:
+			return 'r'
+
+
+	def count_clean(self,slice_array):
+		sub_cantidad = 0
+		clean_size = 0
+		clean_percent = 0.00
+		# print(']')
+		for x in slice_array:
+			sub_cantidad += 1
+			# print(x.state)
+			if x.state == ' ':
+				clean_size += 1
+		# print('[')
+		clean_percent = (clean_size / sub_cantidad)*100
+		# print(' ')
+		# print('Cantidad: ', sub_cantidad)
+		# print('limpios: ', clean_size)
+		# print('porcentaje: ', clean_percent)
+		return clean_percent
+
+
+
+
+if __name__ == "__main__":
+	print('\033[92m')
+	size = random.randint(1, 100)
+
+	position = random.randint(0, size - 1)
+
+	floor = Floor(N=10)
+
+	print("Floor size: ", size)
+	print("Initial position: ", position)
+
+	vacuum = Vacuum(floor=floor, direction='l', position=2)
+	vacuum.run()
+	# floor.print_tiles()
+	# vacuum.cleaning_percentage()
